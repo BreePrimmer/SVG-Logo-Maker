cba17ccff31f893c6e763feca1e24dae2d251ee2
breelovesfrogs <134086243+breelovesfrogs@users.noreply.github.com>
2023-08-19 14:14:25 -0500

    started writing functions to create the different shapes and started a function to write the svg file.

---
+++ index.js
@@ -0,0 +1,11 @@
+const generateSVG = ({text, textcolor, shape, shapecolor}) => (
+    `<?xml version="1.0" standalone="no"?>
+    <svg width="300" height="200" version="1.1" xmlns="http://www.w3.org/2000/svg">
+    <${shape}`
+)
+
+const createTriangle = () => {
+    if (shape == 'Triangle') {
+        let Triangle.shape = 
+    }
+}
