1cc71b73053b57a69e2175fc6df26361345dc1a4
breelovesfrogs <134086243+breelovesfrogs@users.noreply.github.com>
2023-08-21 16:58:35 -0500

    added the shapes class to a separate file and used module.exports

--- index.js
+++ index.js
@@ -1,65 +1,19 @@
 // const shapeFunctions = require('shapes.js')
 const inquirer = require('inquirer')
-
 const fs = require('fs')
-
-class Shape {
-    constructor(text, textcolor, shape, shapecolor) {
-        this.text = text,
-        this.textcolor = textcolor,
-        this.shapecolor = shapecolor
-        this.shape = shape
-    }
-
-    makeShape(shape) {
-        if (shape == 'triangle') {
-            return '<polygon points="150 10, 240 190, 60 190"'
-        } else if (shape == 'circle') {
-            return `<${shape} cx="150" cy="100" r="90"`
-        } else if (shape == 'square') {
-            return '<rect x="60" y="10" width="180" height="180"'
-        }
-    }
-
-    makeText(shape, textcolor, text) {
-        if (shape == 'triangle') {
-            return `<text x="110px" y="150px" font-size="50px" fill="${textcolor}">${text}</text>`
-        } else if (shape == 'circle' || 'square') {
-            return `<text x="90px" y="125px" font-size="70px" fill="${textcolor}">${text}</text>`
-        }
-    }
-
-    // makeTriangle(shape) {
-    //     if (shape == 'triangle') {
-    //         return '<polygon points="150 10, 240 190, 60 190"'
-    //     }
-    // }
-
-    // makeCircle(shape) {
-    //     if (shape == 'circle') {
-    //         return `<${shape} cx="150" cy="100" r="90"`
-    //     }
-    // }
-
-    // makeSquare(shape) {
-    //     if (shape == 'square') {
-    //         return '<rect x="60" y="10" width="180" height=180"'
-    //     }
-    // }
-}
-
-const userShape = new Shape()
-// userShape.makeShape()
+const shapes = require('./lib/shapes')
 
 
+// generates the svg markup
 const generateSVG = ({text, textcolor, shape, shapecolor}) => (
     `<?xml version="1.0" standalone="no"?>
     <svg width="300" height="200" version="1.1" xmlns="http://www.w3.org/2000/svg">
-    ${userShape.makeShape(shape)} fill="${shapecolor}"/>
-    ${userShape.makeText(shape, textcolor, text)}`
+    ${shapes.makeShape(shape)} fill="${shapecolor}"/>
+    ${shapes.makeText(shape, textcolor, text)}`
     
 )
 
+// prompts for user input
 inquirer.prompt([
     {
         type: 'list',
@@ -83,18 +37,11 @@ inquirer.prompt([
         message: 'Please enter the color for your logo text.'
     }
 ]).then((response) => {
+    // generates the markup with the user input response and writes the svg file.
     console.log(response)
-    // const userShape = new Shape(response)
-    // userShape.makeShape()
     const markup = generateSVG(response)
-    console.log(markup)
+    // generates file
     fs.writeFile('./logo.svg', markup, err => {
-        err ? console.error(err) : console.log("File Successfully Written")
+        err ? console.error(err) : console.log("Generated logo.svg")
     });
 });
-
-// const createTriangle = () => {
-//     if (shape == 'Triangle') {
-//         let Triangle.shape = 
-//     }
-// }
